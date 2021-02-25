e2509f6506304d01babcbafb6cee169d0e3513a4
AlexisArenas <dev2@3creatives.com>
2021-02-24 23:51:31 -0500

    commit taller Angular

---
+++ frontend/tallerAngular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
